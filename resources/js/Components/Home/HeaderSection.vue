132a508ebab3445c3395640fb2c53c113cab1a68
unknown <adria.ordis@gmail.com>
2025-08-04 14:40:17 +0200

    header-section

--- resources/js/Components/Home/HeaderSection.vue
+++ resources/js/Components/Home/HeaderSection.vue
@@ -49,45 +49,43 @@ onMounted(async () => {
 
 <template>
 
-<div class="relative z-20 flex max-w-7xl mx-auto -mb-20 px-4 mt-20">
-  <div class="flex w-full md:justify-end justify-start">
+<div class="relative z-20 flex max-w-7xl mx-auto -mb-8 sm:-mb-20 px-4 mt-20 ">
+  <div class="flex w-full justify-end">
     <img 
       src="/images/rotate.webp" 
       alt="Rotate icon"
-      class="w-16 sm:w-20 md:w-28 lg:w-32 animate-slow-spin" 
+      class="w-16 sm:w-20 md:w-24 lg:w-32 animate-slow-spin" 
     />
   </div>
 </div>
 
 
-  <div class="w-full md:h-[839px] h-[600px] bg-[#e9e9e9]">
-    <div class="bg-[url(/images/header.webp)] bg-no-repeat relative overflow-hidden md:h-[839px] h-[600px] w-full flex flex-col md:items-end items-center lg:items-center md:gap-12 gap-6 justify-center">
-  
-  <!-- Capa blanca translúcida només visible en pantalles petites -->
-  <div class="absolute inset-0 bg-white/30 xl:hidden z-0"></div>
-
-  <h1 
-    ref="textRef" 
-    class="relative z-10 text-5xl sm:text-5xl md:text-6xl lg:text-7xl xl:text-[80px] font-bold text-center leading-tight text-black"
-  >
-    <span
-      v-for="(letter, index) in 'UPCYCLING'.split('')"
-      :key="index"
-      class="inline-block"
+  <div class="w-full aspect-[16/9] lg:aspect-auto lg:h-[839px] bg-[#e9e9e9] bg-[url(/images/header.webp)] bg-no-repeat bg-center lg:bg-left bg-cover xl:bg-contain flex items-center justify-around">
+  <div class="w-full max-w-7xl px-6 md:px-12 flex flex-col items-end justify-around gap-6 h-full">
+    <h1 
+      ref="textRef" 
+      class="relative z-10 text-4xl sm:text-5xl md:text-5xl lg:text-[90px] xl:text-[120px] font-bold leading-tight text-left text-black"
     >
-      {{ letter }}
-    </span>
-  </h1>
+      <span
+        v-for="(letter, index) in 'UPCYCLING'.split('')"
+        :key="index"
+        class="inline-block"
+      >
+        {{ letter }}
+      </span>
+    </h1>
 
-  <img 
-    ref="box" 
-    src="/images/bolsas.webp" 
-    alt="Bolsas recicladas"
-    class="relative z-10 h-20 sm:h-24 md:h-24 lg:h-28 xl:h-32 w-auto md:-ml-32 -ml-12 opacity-0 drop-shadow-lg"
-  />
-
-</div>
+    <img 
+      ref="box" 
+      src="/images/bolsas.webp" 
+      alt="Bolsas recicladas"
+      class="relative z-10 h-8 sm:h-24 w-auto opacity-0 drop-shadow-lg"
+    />
   </div>
+</div>
+
+
+
 
 </template>
 
